50b5f8af0f5190d8914c3c231675928f035f7ab3
sandyml <sandramayla@gmail.com>
2023-03-29 17:28:17 -0400

    commit tree afce98f4cd1b3e7664cf0a73fd20893384be2e3d

--- src/reducer.js
+++ src/reducer.js
@@ -1,20 +1,29 @@
-let state = { count: 0 };
+let state;
 
-function changeState(state, action) {
+// changeState reducer
+function changeState(state = { count: 0 }, action) {
   switch (action.type) {
     case "counter/increment":
       return { count: state.count + 1 };
+
     default:
       return state;
   }
 }
 
+// dispatch function 
 function dispatch(action) {
   state = changeState(state, action);
   render();
 }
 
+// render function
 function render() {
   const app = document.querySelector("#app");
   app.textContent = state.count;
 }
+
+// will display in our page display 0 
+// The code changeState(undefined, { type: '@@INIT' }) returns { count: 0 },
+dispatch(undefined, { type: "@@INIT" });
+// dispatch({ type: "counter/increment" });
